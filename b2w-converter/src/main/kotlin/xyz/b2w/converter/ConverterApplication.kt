7761da14eac896ed6018b8f358da25ba695bd6e0
trofimovep <trofimovep42@gmail.com>
2022-12-11 13:06:16 +0100

    fix

--- b2w-converter/src/main/kotlin/xyz/b2w/converter/ConverterApplication.kt
+++ b2w-converter/src/main/kotlin/xyz/b2w/converter/ConverterApplication.kt
@@ -2,7 +2,9 @@ package xyz.b2w.converter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class ConverterApplication
 

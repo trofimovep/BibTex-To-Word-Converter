3df29ceb88d173dee0befa439dffd6c88212d29e
trofimovep <trofimovep42@gmail.com>
2022-12-22 00:00:42 +0100

    add rights to build script

--- build.sh
+++ build.sh
@@ -5,13 +5,19 @@ echo "Starting building the discovery service application..."
 ./discovery-service/gradlew -p discovery-service clean build
 echo "Discovery service application is built!"
 
+echo "......................................................"
+
 echo "Starting building the gateway application..."
 ./gateway/gradlew -p gateway clean build
 echo "Gateway service application is built!"
 
+echo "......................................................"
+
 echo "Starting building the converter application..."
 ./b2w-converter/gradlew -p b2w-converter clean build
 echo "Converter service application is built!"
+
+echo "......................................................"
 
 echo "Starting building the notification service application..."
 ./notification-service/gradlew -p notification-service clean build
